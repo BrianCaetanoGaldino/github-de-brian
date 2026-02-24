51a52944d79f33773d5a1b3e81269422a6687560
unknown <20241pvai0030010@estudantes.ifpr.edu.br>
2026-02-24 09:54:29 -0300

    coisas

---
+++ aulaPOO.py
@@ -0,0 +1,21 @@
+num1 = int(input("digite um numero "))
+
+num2 = int(input("digite mais um numero "))
+   
+sinal = input("digite um sinal (+,-,/,*) ")
+
+if(sinal == "+"):
+    numR = num1 + num2
+    print(numR)
+elif(sinal == "-"):
+    numR = num1 - num2
+    print(numR)
+elif(sinal == "/"):
+    numR = num1 / num2
+    print(numR)
+elif(sinal == "*"):
+    numR = num1 * num2
+    print(numR)
+
+    
+
